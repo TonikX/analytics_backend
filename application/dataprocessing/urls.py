f0482b1932e1461845c3627b3ddecf1165cbbb07
anyaarz <anyaarz97@gmail.com>
2020-06-01 04:12:55 +0300

    rewriting endpoints and parser

--- application/dataprocessing/urls.py
+++ application/dataprocessing/urls.py
@@ -25,36 +25,40 @@ urlpatterns = [
     #url(r'^reset-password/complete/$', password_reset_complete,{'template_name': 'accounts/reset_password_complete.html'},
     #     name='password_reset_complete'),
     
-    url(r'^domain/$', views.DomainListView, name = 'domain'),
-    url(r'^domain/new/$', views.DomainPost.as_view(), name='post_domain'),
-    url(r'^domain/edit/(?P<pk>\d+)/$', views.DomainPostUpdate.as_view(), name='edit_domain'),
-    url(r'^items/$', views.ItemsListView.as_view(), name = 'items'),
-    url(r'^upload/$', views.upload, name = 'upload'),
-    url(r'^items/new/$', views.ItemPost.as_view(), name='post_items'),
-    url(r'^items/edit/(?P<pk>\d+)/$', views.ItemPostUpdate.as_view(), name='edit_items'),
-    url(r'^items/detail/(?P<pk>\d+)/$', views.detail_item, name='details'),
-    url(r'^items/delete/(?P<pk>\d+)/$', views.item_delete, name='items_delete'),
-    url(r'^relation/$', views.RelationListView, name = 'relation'),
-    url(r'^relation/new/$', views.RelationPost.as_view(), name='post_relation'),
-    url(r'^relation/edit/(?P<pk>\d+)/$', views.RelationPostUpdate.as_view(), name='edit_relation'),
+    #url(r'^domain/$', views.DomainListView, name = 'domain'),
+    #url(r'^domain/new/$', views.DomainPost.as_view(), name='post_domain'),
+    #url(r'^domain/edit/(?P<pk>\d+)/$', views.DomainPostUpdate.as_view(), name='edit_domain'),
+    #url(r'^items/$', views.ItemsListView.as_view(), name = 'items'),
+    #url(r'^upload/$', views.upload, name = 'upload'),
+    #url(r'^items/new/$', views.ItemPost.as_view(), name='post_items'),
+    #url(r'^items/edit/(?P<pk>\d+)/$', views.ItemPostUpdate.as_view(), name='edit_items'),
+    #url(r'^items/detail/(?P<pk>\d+)/$', views.detail_item, name='details'),
+    #url(r'^items/delete/(?P<pk>\d+)/$', views.item_delete, name='items_delete'),
+    #url(r'^relation/$', views.RelationListView, name = 'relation'),
+    #url(r'^relation/new/$', views.RelationPost.as_view(), name='post_relation'),
+    #url(r'^relation/edit/(?P<pk>\d+)/$', views.RelationPostUpdate.as_view(), name='edit_relation'),
 
     #
     # API urls
     #
     
 
-    path('api/domain/', views.DomainListCreateAPIView.as_view(), name='domains'),
-    path('api/domain/<int:pk>', views.DomainDetailAPIView.as_view(), name='domain_detail'),
+    path('api/domain/', views.DomainListCreateAPIView.as_view(),),
+    path('api/domain/<int:pk>', views.DomainDetailAPIView.as_view(),),
     
-    path('api/item/', views.ItemsListCreateAPIView.as_view(), name='item'),
-    path('api/item/<int:domain_id>', views.ItemsListAPIView.as_view(), name='itembydomain'),
-    path('api/item/detail/<int:pk>', views.ItemDetailAPIView.as_view(), name='item_detail'),
+    path('api/item/create', views.ItemsListCreateAPIView.as_view(),),
+    path('api/item/', views.ItemsListAPIView.as_view(),),
+    path('api/item/detail/<int:pk>', views.ItemDetailAPIView.as_view(),),
     
     path('api/relation/', views.RelationListCreateAPIView.as_view(),),
     path('api/relation/<int:item1_id>', views.RelationListAPIView.as_view(),),
     path('api/relation/detail/<int:pk>', views.RelationRetrieveDestroyAPIView.as_view(),),
     path('api/relation/update/<int:pk>', views.RelationUpdateAPIView.as_view(),),
     path('api/relation/create', views.RelationPostAPIView.as_view(), ),
+
+    path('api/upload/', views.FileUploadAPIView.as_view(), ),
+
+    
     
 
 
