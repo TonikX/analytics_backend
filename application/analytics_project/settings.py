dd4cb7ac31489f9499614a75b8934223a5fb2137
rendex85 <rendex85@gmail.com>
2020-07-30 10:08:43 +0300

    small changes in settings

--- application/analytics_project/settings.py
+++ application/analytics_project/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'analytics_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -108,19 +108,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        # 'PASSWORD': 'qwe123',
-        # 'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+
 
 
 # Password validation
