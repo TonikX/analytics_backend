19d617a6f20e5e7b3f8009a9e79f288e91ada527
TonikX <antongovorov@gmail.com>
2020-10-22 22:37:27 +0300

    all pyc files removed

--- application/workprogramsapp/models.py
+++ application/workprogramsapp/models.py
@@ -57,7 +57,6 @@ class WorkProgram(CloneMixin, models.Model):
                                      blank=True, null=True)
     prerequisites = models.ManyToManyField(Items, related_name='WorkProgramPrerequisites',
                                            through='PrerequisitesOfWorkProgram', blank=True, null=True,
-                                           git rm *.pyc     verbose_name="Пререквизиты")
     outcomes = models.ManyToManyField(Items, related_name='WorkProgramOutcomes', through='OutcomesOfWorkProgram',
                                       verbose_name="Постреквизиты")
     title = models.CharField(max_length=1024, verbose_name="Название")
