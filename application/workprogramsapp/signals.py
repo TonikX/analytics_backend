5e6e77dc47f2ba8b889be169120fad8fb68b9149
rendex85 <rendex85@gmail.com>
2020-12-08 18:21:49 +0300

    academic plan dev add

--- application/workprogramsapp/signals.py
+++ application/workprogramsapp/signals.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import Group
 
 def populate_models(sender, **kwargs):
     list_of_groups = ["rpd_developer", "education_plan_developer", "op_leader", "roles_and_professions_master",
-                      "student", "expertise_master"]
+                      "student", "expertise_master", "academic_plan_developer"]
     for group in list_of_groups:
         Group.objects.get_or_create(name=group)
