26044f8a8f350ddaf3de91a5427157faad365e8a
Valeriya Artamonova <lerartlera@gmail.com>
2021-03-18 18:34:18 +0300

    added ordering, search to onlinecourse

--- application/onlinecourse/views.py
+++ application/onlinecourse/views.py
@@ -1,9 +1,9 @@
+from rest_framework import filters
+from rest_framework import generics, permissions, viewsets, renderers
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Institution, Platform, OnlineCourse, CourseCredit, CourseRequirement, CourseFieldOfStudy,\
     CourseLearningOutcome, CourseWorkProgram
-from django.shortcuts import render
-from rest_framework import generics, permissions, viewsets, renderers
-from django_filters.rest_framework import DjangoFilterBackend
 from .service import OnlineCourseFilter
 from .serializers import InstitutionSerializer, PlatformSerializer, OnlineCourseSerializer, \
     CourseCreditSerializer, CourseRequirementSerializer, CourseFieldOfStudySerializer, CourseLearningOutcomeSerializer,\
@@ -14,20 +14,24 @@ class InstitutionViewSet(viewsets.ModelViewSet):
     """Контроллер для модели Правообладатель"""
     queryset = Institution.objects.all()
     serializer_class = InstitutionSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
 
 
 class PlatformViewSet(viewsets.ModelViewSet):
     """Контроллер для модели Платформа"""
     queryset = Platform.objects.all()
     serializer_class = PlatformSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
 
 
 class OnlineCourseViewSet(viewsets.ModelViewSet):
     """Контроллер для модели Онлайн курс"""
     queryset = OnlineCourse.objects.all()
     serializer_class = OnlineCourseSerializer
-    filter_backends = (DjangoFilterBackend,
-                       )
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    search_fields = ['title']
     filterset_class = OnlineCourseFilter
 
 
