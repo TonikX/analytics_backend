19ae398d7d503deb92ce2a5c5d3fdec662cdc45d
Poline <krasnova.poline@gmail.com>
2021-03-21 16:52:09 +0300

    return courses edits

--- frontend/src/components/Menu/MenuList.tsx
+++ frontend/src/components/Menu/MenuList.tsx
@@ -79,12 +79,12 @@ export default (groups: Array<string>) => {
             icon: WorkProgramIcon,
             selectedIcon: WorkProgramSelectedIcon,
         },
-        // {
-        //     title: 'Онлайн курсы',
-        //     link: appRouter.getCoursesRoute(),
-        //     icon: CoursesIcon,
-        //     selectedIcon: CoursesSelectedIcon,
-        // },
+        {
+            title: 'Онлайн курсы',
+            link: appRouter.getCoursesRoute(),
+            icon: CoursesIcon,
+            selectedIcon: CoursesSelectedIcon,
+        },
         {
             title: 'Онлайн курсы',
             link: appRouter.getOnlineCoursesRoute(),
