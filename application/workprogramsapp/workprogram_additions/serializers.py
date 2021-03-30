c2befe17213dcd0feee93e0e6d1c51258eb66a6a
TonikX <antongovorov@gmail.com>
2021-03-30 19:12:56 +0300

    fix workprogram fields

--- application/workprogramsapp/workprogram_additions/serializers.py
+++ application/workprogramsapp/workprogram_additions/serializers.py
@@ -85,3 +85,13 @@ class StructuralUnitSerializer(serializers.ModelSerializer):
         model = StructuralUnit
         fields = "__all__"
 
+
+class ShortStructuralUnitSerializer(serializers.ModelSerializer):
+    """
+    Cериализатор подразделения разработчика РПД
+    """
+
+
+    class Meta:
+        model = StructuralUnit
+        fields = "__all__"
