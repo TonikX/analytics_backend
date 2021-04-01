017fcfae2fb57342bc34c5452c1245b2281566ee
Valeriya Artamonova <lerartlera@gmail.com>
2021-04-01 21:21:39 +0300

    Update views.py

--- application/onlinecourse/views.py
+++ application/onlinecourse/views.py
@@ -1,13 +1,17 @@
-from rest_framework import filters
-from rest_framework import generics, permissions, viewsets, renderers
 from django_filters.rest_framework import DjangoFilterBackend
-from django.db import models
+from rest_framework import filters
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from .models import Institution, Platform, OnlineCourse, CourseCredit, CourseRequirement, CourseFieldOfStudy,\
+
+from .models import Institution, Platform, OnlineCourse, CourseCredit, CourseRequirement, CourseFieldOfStudy, \
     CourseLearningOutcome, CourseWorkProgram
 from .serializers import InstitutionSerializer, PlatformSerializer, OnlineCourseSerializer, \
-    CourseCreditSerializer, CourseRequirementSerializer, CourseFieldOfStudySerializer, CourseLearningOutcomeSerializer,\
+    CourseCreditSerializer, CourseRequirementSerializer, CourseFieldOfStudySerializer, CourseLearningOutcomeSerializer, \
     CourseWorkProgramSerializer
+
+from .data_onlinecourse import data_platform, data_rigthholder, data_online_course_platform_inst
 
 
 class InstitutionViewSet(viewsets.ModelViewSet):
@@ -33,7 +37,7 @@ class OnlineCourseViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
     search_fields = ['title', 'platform__title', 'institution__title']
     ordering_fields = ['title', 'platform__title', 'institution__title', 'language', 'started_at', 'rating']
-    filterset_fields = ['platform__title', 'institution__title', 'language']
+    filterset_fields = ['platform__title', 'institution__title', 'language', 'course__field_of_study__number', 'course__field_of_study__title']
 
 
 class CourseCreditViewSet(viewsets.ModelViewSet):
@@ -64,3 +68,48 @@ class CourseWorkProgramViewSet(viewsets.ModelViewSet):
     """Контроллер для модели РПД и онлайн курс"""
     queryset = CourseWorkProgram.objects.all()
     serializer_class = CourseWorkProgramSerializer
+
+
+"""Загрузка данных из реестра онлайн курсов """
+
+
+class CourseDataAPIView(APIView):
+    """
+
+    """
+    def post(self, request):
+        for i in range(len(data_platform)):
+            platform = Platform.objects.create(id=data_platform.index[i],
+                                               title=data_platform.title[i],)
+            platform.save()
+        for i in range(len(data_rigthholder)):
+            institution = Institution.objects.create(id=data_rigthholder.index[i],
+                                                     title=data_rigthholder.title[i],)
+            institution.save()
+        for i in range(len(data_online_course_platform_inst)):
+            onlinecourse = OnlineCourse.objects.create(id=data_online_course_platform_inst.index[i],
+                                                       title=data_online_course_platform_inst.title_x[i],
+                                                       description=data_online_course_platform_inst.description[i],
+                                                       institution=Institution.objects.get(id=data_online_course_platform_inst.id_rightholder[i]),
+                                                       platform=Platform.objects.get(id=data_online_course_platform_inst.id_platform[i]),
+                                                       language=data_online_course_platform_inst.language[i],
+                                                       started_at=data_online_course_platform_inst.started_at[i],
+                                                       created_at=data_online_course_platform_inst.created_at[i],
+                                                       record_end_at=data_online_course_platform_inst.record_end_at[i],
+                                                       finished_at=data_online_course_platform_inst.finished_at[i],
+                                                       rating=data_online_course_platform_inst.rating[i],
+                                                       experts_rating=data_online_course_platform_inst.experts_rating[i],
+                                                       visitors_number=data_online_course_platform_inst.visitors_number[i],
+                                                       total_visitors_number=data_online_course_platform_inst.total_visitors_number[i],
+                                                       duration=data_online_course_platform_inst.duration[i],
+                                                       volume=data_online_course_platform_inst.volume[i],
+                                                       intensity_per_week=data_online_course_platform_inst.intensity_per_week[i],
+                                                       content=data_online_course_platform_inst.content[i],
+                                                       lectures_number=data_online_course_platform_inst.lectures_number[i],
+                                                       external_url=data_online_course_platform_inst.external_url[i],
+                                                       has_certificate=data_online_course_platform_inst.has_certificate[i],
+                                                       credits=data_online_course_platform_inst.credits[i],
+                                                       )
+            onlinecourse.save()
+        return Response(status=200)
+
