3e08ddafe2145e3fbcd245c00158efe9e7d76479
Valeriya Artamonova <lerartlera@gmail.com>
2021-04-04 02:21:45 +0300

    Update views.py

--- application/onlinecourse/views.py
+++ application/onlinecourse/views.py
@@ -11,7 +11,7 @@ from .serializers import InstitutionSerializer, PlatformSerializer, OnlineCourse
     CourseCreditSerializer, CourseRequirementSerializer, CourseFieldOfStudySerializer, CourseLearningOutcomeSerializer, \
     CourseWorkProgramSerializer
 
-from .data_onlinecourse import data_platform, data_rigthholder, data_online_course_platform_inst
+from .data_onlinecourse import get_data
 
 
 class InstitutionViewSet(viewsets.ModelViewSet):
@@ -78,6 +78,7 @@ class CourseDataAPIView(APIView):
 
     """
     def post(self, request):
+        data_platform, data_rigthholder, data_online_course = get_data()
         for i in range(len(data_platform)):
             if Platform.objects.filter(id=data_platform.index[i]).exists():
                 continue
@@ -92,33 +93,64 @@ class CourseDataAPIView(APIView):
                 institution = Institution.objects.create(id=data_rigthholder.index[i],
                                                          title=data_rigthholder.title[i],)
                 institution.save()
-        for i in range(len(data_online_course_platform_inst)):
-            if OnlineCourse.objects.filter(id=data_online_course_platform_inst.index[i]).exists():
+        for i in range(len(data_online_course)):
+            if OnlineCourse.objects.filter(id=data_online_course.index[i]).exists():
                 continue
             else:
-                onlinecourse = OnlineCourse.objects.create(id=data_online_course_platform_inst.index[i],
-                                                           title=data_online_course_platform_inst.title_x[i],
-                                                           description=data_online_course_platform_inst.description[i],
-                                                           institution=Institution.objects.get(id=data_online_course_platform_inst.id_rightholder[i]),
-                                                           platform=Platform.objects.get(id=data_online_course_platform_inst.id_platform[i]),
-                                                           language=data_online_course_platform_inst.language[i],
-                                                           started_at=data_online_course_platform_inst.started_at[i],
-                                                           created_at=data_online_course_platform_inst.created_at[i],
-                                                           record_end_at=data_online_course_platform_inst.record_end_at[i],
-                                                           finished_at=data_online_course_platform_inst.finished_at[i],
-                                                           rating=data_online_course_platform_inst.rating[i],
-                                                           experts_rating=data_online_course_platform_inst.experts_rating[i],
-                                                           visitors_number=data_online_course_platform_inst.visitors_number[i],
-                                                           total_visitors_number=data_online_course_platform_inst.total_visitors_number[i],
-                                                           duration=data_online_course_platform_inst.duration[i],
-                                                           volume=data_online_course_platform_inst.volume[i],
-                                                           intensity_per_week=data_online_course_platform_inst.intensity_per_week[i],
-                                                           content=data_online_course_platform_inst.content[i],
-                                                           lectures_number=data_online_course_platform_inst.lectures_number[i],
-                                                           external_url=data_online_course_platform_inst.external_url[i],
-                                                           has_certificate=data_online_course_platform_inst.has_certificate[i],
-                                                           credits=data_online_course_platform_inst.credits[i],
+                onlinecourse = OnlineCourse.objects.create(id=data_online_course.index[i],
+                                                           title=data_online_course.title_x[i],
+                                                           description=data_online_course.description[i],
+                                                           institution=Institution.objects.get(id=data_online_course.id_rightholder[i]),
+                                                           platform=Platform.objects.get(id=data_online_course.id_platform[i]),
+                                                           language=data_online_course.language[i],
                                                            )
                 onlinecourse.save()
+            if data_online_course.started_at[i] != 'None':
+                onlinecourse.started_at = data_online_course.started_at[i]
+                onlinecourse.save()
+            if data_online_course.created_at[i] != 'None':
+                onlinecourse.created_at = data_online_course.created_at[i]
+                onlinecourse.save()
+            if data_online_course.record_end_at[i] != 'None':
+                onlinecourse.record_end_at = data_online_course.record_end_at[i]
+                onlinecourse.save()
+            if data_online_course.finished_at[i] != 'None':
+                onlinecourse.finished_at = data_online_course.finished_at[i]
+                onlinecourse.save()
+            if data_online_course.rating[i] != 'None':
+                onlinecourse.rating = data_online_course.rating[i]
+                onlinecourse.save()
+            if data_online_course.experts_rating[i] != 'None':
+                onlinecourse.experts_rating = float(data_online_course.experts_rating[i])
+                onlinecourse.save()
+            if data_online_course.visitors_number[i] != 'None':
+                onlinecourse.visitors_number = int(data_online_course.visitors_number[i])
+                onlinecourse.save()
+            if data_online_course.total_visitors_number[i] != 'None':
+                onlinecourse.total_visitors_number = int(data_online_course.total_visitors_number[i])
+                onlinecourse.save()
+            if data_online_course.duration[i] != 'None':
+                onlinecourse.duration = int(data_online_course.duration[i])
+                onlinecourse.save()
+            if data_online_course.volume[i] != 'None':
+                onlinecourse.volume = int(data_online_course.volume[i])
+                onlinecourse.save()
+            if data_online_course.intensity_per_week[i] != 'None':
+                onlinecourse.intensity_per_week = int(data_online_course.intensity_per_week[i])
+                onlinecourse.save()
+            if data_online_course.content[i] != 'None':
+                onlinecourse.content = str(data_online_course.content[i])
+                onlinecourse.save()
+            if data_online_course.lectures_number[i] != 'None':
+                onlinecourse.lectures_number = int(data_online_course.lectures_number[i])
+                onlinecourse.save()
+            if data_online_course.external_url[i] != 'None':
+                onlinecourse.external_url = str(data_online_course.external_url[i])
+                onlinecourse.save()
+            if data_online_course.has_certificate[i] != 'None':
+                onlinecourse.has_certificate = bool(data_online_course.has_certificate[i])
+                onlinecourse.save()
+            if data_online_course.credits[i] != 'None':
+                onlinecourse.credits = float(data_online_course.credits[i])
+                onlinecourse.save()
         return Response(status=200)
-
