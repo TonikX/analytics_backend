c4f7042e2ef3f03face88c863bfe62d520ae6f87
rendex85 <rendex85@gmail.com>
2021-04-19 17:27:48 +0300

    info about empty string + smmall fix

--- application/workprogramsapp/workprogram_additions/views.py
+++ application/workprogramsapp/workprogram_additions/views.py
@@ -5,7 +5,7 @@ from django.db.models import Count
 from rest_framework import filters
 from rest_framework import generics, viewsets
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
 # Сериализаторы
@@ -18,8 +18,9 @@ from .models import AdditionalMaterial, StructuralUnit, UserStructuralUnit
 
 # Права доступа
 from workprogramsapp.permissions import IsRpdDeveloperOrReadOnly
-from ..models import WorkProgram, DisciplineSection, PrerequisitesOfWorkProgram, OutcomesOfWorkProgram
-from ..serializers import WorkProgramSerializer
+from ..models import WorkProgram, DisciplineSection, PrerequisitesOfWorkProgram, OutcomesOfWorkProgram, \
+    WorkProgramInFieldOfStudy
+from ..serializers import WorkProgramSerializer, WorkProgramShortForExperiseSerializer
 
 
 class AdditionalMaterialSet(viewsets.ModelViewSet):
@@ -72,7 +73,7 @@ class UserStructuralUnitSet(viewsets.ModelViewSet):
         return UserStructuralUnitSerializer
 
 @api_view(['POST'])
-@permission_classes((IsAuthenticated,))
+@permission_classes((IsRpdDeveloperOrReadOnly,))
 def CopyContentOfWorkProgram(request):
     """
     API-запрос на компирование содержимого из одной РПД в другую, !!!при этом старая РПД удаляется!!!
@@ -87,14 +88,28 @@ def CopyContentOfWorkProgram(request):
         new_wp.approval_date = old_wp.approval_date
         new_wp.authors = old_wp.authors
 
+        new_items = PrerequisitesOfWorkProgram.objects.filter(workprogram=to_copy)
+        new_out = OutcomesOfWorkProgram.objects.filter(workprogram=to_copy)
+
         for item in PrerequisitesOfWorkProgram.objects.filter(workprogram=from_copy):
-            item.workprogram = new_wp
-            item.save()
+            item_exists = False
+            for new_item in new_items:
+                if new_item.item == item.item and new_item.masterylevel == item.masterylevel:
+                    print(item_exists)
+                    item_exists = True
+            if not item_exists:
+                item.workprogram = new_wp
+                item.save()
 
         for item in OutcomesOfWorkProgram.objects.filter(workprogram=from_copy):
-            item.workprogram = new_wp
-            item.save()
-
+            item_exists = False
+            for new_item in new_out:
+                if new_item.item == item.item:
+                    print(item_exists)
+                    item_exists = True
+            if not item_exists:
+                item.workprogram = new_wp
+                item.save()
         new_wp.editors.add(*old_wp.editors.all())
         new_wp.bibliographic_reference.add(*old_wp.bibliographic_reference.all())
 
@@ -111,9 +126,23 @@ def CopyContentOfWorkProgram(request):
         for section in DisciplineSection.objects.filter(work_program__pk=from_copy):
             section.work_program = new_wp
             section.save()
-        old_wp.delete()
+        # old_wp.delete()
         new_wp.save()
         serializer = WorkProgramSerializer(new_wp, many=False)
+        return Response(serializer.data)
+    except:
+        return Response(status=400)
+
+
+@api_view(['GET'])
+@permission_classes((AllowAny,))
+def EmptyStringWp(request):
+    """
+    API-запрос на просмотр ВП, которых нету в УП
+    """
+    try:
+        empty_wp = WorkProgram.objects.filter(editors__isnull=False, zuns_for_wp__id_str_up__isnull=True).distinct()
+        serializer = WorkProgramShortForExperiseSerializer(empty_wp, many=True)
         return Response(serializer.data)
     except:
         return Response(status=400)
