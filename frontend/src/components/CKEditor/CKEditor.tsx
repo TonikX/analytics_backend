86e684b7721a14fcb5914dd4a84c7a1ff975f47e
Nikita <fnik340@gmail.com>
2021-05-12 20:12:18 +0300

    fix open link

--- frontend/src/components/CKEditor/CKEditor.tsx
+++ frontend/src/components/CKEditor/CKEditor.tsx
@@ -9,9 +9,10 @@ export const CKEditorComponent: React.FC<CKEditorProps> =
     ({ value, onChange, useFormulas, onFocus, readOnly = false, onBlur, toolbarIcons, 
         toolbarContainerId = 'toolbar-container', height, width }) => {
     const config = {
-        extraPlugins: [useFormulas ? 'mathjax' : '', 'embed',  'autoembed', 'font', 'justify'],
+        extraPlugins: [useFormulas ? 'mathjax' : '', 'embed',  'autoembed', 'font', 'justify', 'openlink'],
         embed_provider: '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}',
         mathJaxLib: '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-AMS_HTML',
+        openlink_enableReadOnly: true,
         height,
         width,
     }
@@ -23,6 +24,10 @@ export const CKEditorComponent: React.FC<CKEditorProps> =
                 onBlur={onBlur}
                 onFocus={onFocus}
                 readOnly={readOnly}
+                onBeforeLoad={(CKEDITOR: any) => {
+                    CKEDITOR.plugins.addExternal( 'openlink', '/openlink/plugin.js');
+                }
+                }
             />
     )
 }
