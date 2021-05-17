fc662b708c8f626eced78df5affcfecf5ba8f8e7
TonikX <antongovorov@gmail.com>
2021-05-17 10:57:37 +0300

    new field for ap

--- application/workprogramsapp/serializers.py
+++ application/workprogramsapp/serializers.py
@@ -498,9 +498,20 @@ class DisciplineBlockSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'modules_in_discipline_block']
 
 
+
+
+class ImplementationAcademicPlanShortForAPSerializer(serializers.ModelSerializer):
+    field_of_study = FieldOfStudyImplementationSerializer(many=True)
+
+    class Meta:
+        model = ImplementationAcademicPlan
+        fields = ['id', 'year', 'qualification', 'title', 'field_of_study']
+
+
 class AcademicPlanSerializer(serializers.ModelSerializer):
     discipline_blocks_in_academic_plan = DisciplineBlockSerializer(many=True, required=False)
     can_edit = BooleanField(read_only=True)
+    academic_plan_in_field_of_study = ImplementationAcademicPlanShortForAPSerializer()
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
@@ -692,14 +703,6 @@ class AcademicPlanSerializerForList(serializers.ModelSerializer):
     class Meta:
         model = AcademicPlan
         fields = ['id', 'educational_profile', 'number', 'approval_date', 'year', 'education_form', 'qualification']
-
-
-class ImplementationAcademicPlanShortForAPSerializer(serializers.ModelSerializer):
-    field_of_study = FieldOfStudyImplementationSerializer(many=True)
-
-    class Meta:
-        model = ImplementationAcademicPlan
-        fields = ['id', 'year', 'qualification', 'title', 'field_of_study']
 
 
 class AcademicPlanShortSerializer(serializers.ModelSerializer):
