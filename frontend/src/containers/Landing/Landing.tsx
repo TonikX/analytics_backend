37d0ba5f20be3c237060cca78748a6834800f3d4
Poline <krasnova.poline@gmail.com>
2021-06-22 00:26:14 +0300

    update landing

--- frontend/src/containers/Landing/Landing.tsx
+++ frontend/src/containers/Landing/Landing.tsx
@@ -14,8 +14,11 @@ import teacherImage from './img/teacher.svg'
 import administrationImage from './img/administration.svg'
 import arrowImage from './img/right-arrow.svg'
 import mailImage from './img/envelope.svg'
+import logo from './img/logo.svg'
 import {appRouter} from '../../service/router-service'
 import developersList from "./developersList";
+// @ts-ignore
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 export default () => {
   const classes = useStyles();
@@ -27,11 +30,11 @@ export default () => {
         <div className={classes.headerWrap}>
 
           <Typography className={classes.headerLeft}>
-            <Typography className={classes.logo}> LOGO </Typography>
-            <Link to='' className={classes.link} style={{marginRight: '25px'}}>Внешние системы</Link>
-            <Link to='' className={classes.link} style={{marginRight: '25px'}}>Связанные процессы</Link>
-            <Link to='' className={classes.link} style={{marginRight: '25px'}}>Категории пользователей</Link>
-            <Link to='' className={classes.link} style={{marginRight: '25px'}}>Команда</Link>
+            <Typography className={classes.logo}> <img src={logo} width={70} /> </Typography>
+            <AnchorLink offset='100' href='#systems' className={classes.link} style={{marginRight: '25px'}}>Внешние системы</AnchorLink>
+            <AnchorLink offset='100' href='#processes' className={classes.link} style={{marginRight: '25px'}}>Связанные процессы</AnchorLink>
+            <AnchorLink offset='100' href='#users' className={classes.link} style={{marginRight: '25px'}}>Категории пользователей</AnchorLink>
+            <AnchorLink offset='100' href='#developers' className={classes.link} style={{marginRight: '25px'}}>Команда</AnchorLink>
           </Typography>
 
           <Typography className={classes.headerRight}>
@@ -64,7 +67,11 @@ export default () => {
         <Typography variant="h1" className={classes.h1}>Конструктор Рабочих Программ</Typography>
         <Typography className={classes.subTitle}>Удобный конструктор для быстрого создания рабочих программ, отличный инструмент <br/> для сопровождения и планирования учебного процесса<br/></Typography>
         <div className={classes.mainScreenButtons}>
-          <Button className={classes.mainButton}>Посмотреть демо</Button>
+          <Button className={classes.mainButton}>
+            <Link to={appRouter.getSignInRoute()}>
+              Войти в систему
+            </Link>
+          </Button>
           <Button className={classes.secondaryButton}>
             <Link to={appRouter.getSignUpRoute()}>
               Зарегистрироваться
@@ -75,7 +82,7 @@ export default () => {
 
       <div className={classes.screenshot} style={{backgroundImage: `url(${screenshotImage}`}} />
 
-      <div className={classes.section}>
+      <section className={classes.section} id="systems">
         <Typography className={classes.sectionTitle}>Внешние системы</Typography>
         <div className={classes.partnersList}>
           <Typography>ИСУ</Typography>
@@ -83,9 +90,9 @@ export default () => {
           <Typography>БАРС 2.0</Typography>
           <Typography>УЧЕБНАЯ АНАЛИТИКА</Typography>
         </div>
-      </div>
+      </section>
 
-      <div className={classes.section}>
+      <section className={classes.section} id="processes">
         <Typography className={classes.sectionTitle}> Связанные с РПД процессы </Typography>
 
         <div className={classes.processesListItem}>
@@ -94,7 +101,7 @@ export default () => {
               Рабочая программа
             </Typography>
             <Typography className={classes.processesListContentText}>
-              Формирование и редактирование РПД, формальная верификация, содержательная экспертиза, внесение таблицы с баллами в ЦДО/БАРС 2.0,
+              Сервис предоставляет интерфейсы для заполнения всех элементов РПД, таких, как: разделы, темы, оценочные средства, таблица БАРС и т.д. Настроены различные проверки, помогающие преподавателю быстро и правильно заполнять РПД. Реализованы процессы экспертизы для включения РПД в учебные планы.
             </Typography>
           </div>
           <div className={classes.processesListContentImage} style={{backgroundImage: `url(${screenshotImage}`}}/>
@@ -107,7 +114,7 @@ export default () => {
               Учебные планы
             </Typography>
             <Typography className={classes.processesListContentText}>
-              Cоздание учебного плана, оценка качества учебного плана, проверка наличия рабочих программ по дисциплинам учебного плана, включение РПД в учебный план
+              Добавлены функционал работы с учебными планами, блоками, модулями. Учтены все особенности работы над учебными планами в университете ИТМО. Для РПД, включаемой в учебный план возможно настроить тип выборности, компетенции, индикаторы, результаты обучения и другие параметры
             </Typography>
           </div>
         </div>
@@ -115,17 +122,29 @@ export default () => {
         <div className={classes.processesListItem}>
           <div className={classes.processesListContent}>
             <Typography className={classes.processesListContentTitle}>
-              Образовательная программа
+              Образовательные программы
             </Typography>
             <Typography className={classes.processesListContentText}>
-              Cоздание учебного плана, оценка качества учебного плана, проверка наличия рабочих программ по дисциплинам учебного плана, включение РПД в учебный план
+              Реализуются новые интерфейсы для работы с направлениями, учебными планами, общими характеристиками образовательных программ. Реализованы удобные интерфейсы поиска и навигации. Формат документов соответствует образовательному стандарту Университета ИТМО
             </Typography>
           </div>
           <div className={classes.processesListContentImage} style={{backgroundImage: `url(${screenshotImage}`}} />
         </div>
-      </div>
 
-      <div className={classes.section}>
+        <div className={classes.processesListItem}>
+          <div className={classes.processesListContentImage} style={{marginRight: '50px', backgroundImage: `url(${screenshotImage}`}} />
+          <div className={classes.processesListContent}>
+            <Typography className={classes.processesListContentTitle}>
+              Онлайн курсы
+            </Typography>
+            <Typography className={classes.processesListContentText}>
+              В сервисе реализован удобный навигатор по онлайн курсам и возможность их добавления в РПД. Онлайн курсы соответствуют Реестр онлайн-курсов современной цифровой образовательной среды в РФ (<a style={{color: '#fff'}} href={"https://online.edu.ru/"}>https://online.edu.ru/</a>)
+            </Typography>
+          </div>
+        </div>
+      </section>
+
+      <section className={classes.section} id="users">
         <Typography className={classes.sectionTitle}>
           Категории пользователей
         </Typography>
@@ -143,9 +162,9 @@ export default () => {
             <Typography className={classes.usersListItemTitle}> Административные работники </Typography>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div className={classes.section}>
+      <section className={classes.section} id="developers">
         <Typography className={classes.sectionTitle}>
           Команда проекта
         </Typography>
@@ -173,7 +192,7 @@ export default () => {
           </div>
         }
         {!showAllTeam && <Typography className={classes.showAllTeam} onClick={() => setShowAllTeam(true)}>Показать всю команду <img src={arrowImage} className={classes.arrowImage}/></Typography>}
-      </div>
+      </section>
       <div className={classes.footer}>
         <div className={classes.footerWrap}>
           <Typography className={classes.footerText}>Разработка © 2021 Факультет ИКТ</Typography>
