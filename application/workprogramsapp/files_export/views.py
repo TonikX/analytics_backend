1625a38309d8281bb48cb90cb793bfa323ed3d1d
Valeriya Artamonova <lerartlera@gmail.com>
2021-07-05 22:15:34 +0300

    поправлен баг с выгрузкой рпд

--- application/workprogramsapp/files_export/views.py
+++ application/workprogramsapp/files_export/views.py
@@ -133,23 +133,28 @@ def render_context(context, **kwargs):
     template_context['current_evaluation_tool'] = current_evaluation_tool
     certification_evaluation_tools = []
     for item in context['certification_evaluation_tools']:
-        items_max.append(item['max'])
-        items_min.append(item['min'])
-        item['description'] = html2text.html2text(item['description'])
-        if item['type'] == '1':
-            item['type'] = 'Exam'
-        elif item['type'] == '2':
-            item['type'] = 'Differentiated credit'
-        elif item['type'] == '3':
-            item['type'] = 'Offset'
-        elif item['type'] == '4':
-            item['type'] = 'Coursework'
-        certification_evaluation_tools.append(item)
+        try:
+            if item['max'] is not None:
+                items_max.append(item['max'])
+            if item['min'] is not None:
+                items_min.append(item['min'])
+            item['description'] = html2text.html2text(item['description'])
+            if item['type'] == '1':
+                item['type'] = 'Exam'
+            elif item['type'] == '2':
+                item['type'] = 'Differentiated credit'
+            elif item['type'] == '3':
+                item['type'] = 'Offset'
+            elif item['type'] == '4':
+                item['type'] = 'Coursework'
+            certification_evaluation_tools.append(item)
+        except:
+            continue
     template_context['certification_evaluation_tools'] = certification_evaluation_tools
-    outcomes_max_all = sum(items_max)
-    outcomes_min_all = sum(items_min)
-    template_context['outcomes_max_all'] = outcomes_max_all
-    template_context['outcomes_min_all'] = outcomes_min_all
+    template_context['outcomes_max_all'] = sum(items_max) + int(context['extra_points'])
+    template_context['outcomes_min_all'] = sum(items_min)
+    template_context['extra_points'] = context['extra_points']
+    print('extra points - - -- - ', context['extra_points'])
     return template_context, filename
 
 
