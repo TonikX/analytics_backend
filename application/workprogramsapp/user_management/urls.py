4f35aee00f8a672e9cf48c05dedabb7a3c11cee1
rendex85 <rendex85@gmail.com>
2021-09-13 18:39:20 +0300

    crud interfaces for user

--- application/workprogramsapp/user_management/urls.py
+++ application/workprogramsapp/user_management/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from workprogramsapp.user_management.views import UserListView
+from workprogramsapp.user_management.views import UserListView, UserRetrieveView
 
 urlpatterns = [
 
     path('api/user_management/user_list', UserListView.as_view()),
+    path('api/user_management/crud_single_user/<int:pk>', UserRetrieveView.as_view()),
 ]
--- application/workprogramsapp/user_management/views.py
+++ application/workprogramsapp/user_management/views.py
@@ -1,16 +1,23 @@
 # Сериализаторы
-from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics
 from rest_framework.permissions import IsAuthenticated
 
 # Права доступа
 from dataprocessing.models import User
-
-
 # Сериализаторы
 from workprogramsapp.user_management.serializers import UserSerializer
 
 
-class UserListView(ListAPIView):
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    search_fields = ['username', 'first_name', 'last_name', 'email', 'isu_number', "groups", "groups_name"]
+
+
+class UserRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
