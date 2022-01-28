295b958a81cd9f5d828f93f6228bb307021b29db
Polina <krasnova.poline@gmail.com>
2022-01-28 11:50:16 +0300

    Add new plan button was returned

--- frontend/src/containers/EducationalPlan/EducationalPlan.connect.ts
+++ frontend/src/containers/EducationalPlan/EducationalPlan.connect.ts
@@ -7,8 +7,8 @@ import {getEducationalPlan, getCurrentPage, getSearchQuery, getAllCount, getSort
 import {EducationalPlanActions} from "./types";
 
 import {rootState} from "../../store/reducers";
-// import {isUserInOpGroup} from "../../common/userRights";
-// import {getUserGroups} from "../../layout/getters";
+import {isUserInOpGroup} from "../../common/userRights";
+import {getUserGroups} from "../../layout/getters";
 
 const mapStateToProps = (state: rootState) => {
     return {
@@ -18,8 +18,7 @@ const mapStateToProps = (state: rootState) => {
         allCount: getAllCount(state),
         sortingField: getSortingField(state),
         sortingMode: getSortingMode(state),
-        canAddNewPlan: false
-        // canAddNewPlan: isUserInOpGroup(getUserGroups(state))
+        canAddNewPlan: isUserInOpGroup(getUserGroups(state))
     };
 };
 
