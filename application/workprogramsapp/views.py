7567bd51ffb321adfbcda333b1bb9c9ae2f186eb
TonikX <antongovorov@gmail.com>
2022-03-28 10:35:38 +0300

    fix editors update backend

--- application/workprogramsapp/views.py
+++ application/workprogramsapp/views.py
@@ -523,6 +523,21 @@ class WorkProgramEditorsUpdateView(generics.UpdateAPIView):
     permission_classes = [IsExpertiseMaster, IsRpdDeveloperOrReadOnly]
 
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(WorkProgramSerializer(instance).data)
+
+
 class WorkProgramDetailsView(generics.RetrieveAPIView):
     queryset = WorkProgram.objects.all()
     serializer_class = WorkProgramSerializer
