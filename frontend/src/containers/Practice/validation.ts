1c086edbcfc9eac1945a6a5da5b54f0e911810ce
denisrezni <denisrezni@yandex-team.ru>
2022-04-30 11:40:00 +0300

    validate year

--- frontend/src/containers/Practice/validation.ts
+++ frontend/src/containers/Practice/validation.ts
@@ -9,15 +9,23 @@ const presence = {
 }
 
 const validationRules = {
-    [PracticeFields.TITLE] : {
+    [PracticeFields.TITLE]: {
         presence,
     },
-    [PracticeFields.AUTHORS] : {
+    [PracticeFields.AUTHORS]: {
         presence,
     },
     [PracticeFields.OP_LEADER]: {
         presence,
     },
+    [PracticeFields.YEAR]: {
+        presence,
+        range: {
+            min: 1990,
+            max: 2030,
+            message: 'Введите год между 1990 и 2030',
+        }
+    }
 } as any;
 
 export const validate = (field: PracticeFields, value: string): Error => {
@@ -29,6 +37,28 @@ export const validate = (field: PracticeFields, value: string): Error => {
             return {
                 message: presence.message,
             };
+        }
+    }
+
+    if (rules.range) {
+        if (Number.isNaN(Number(value))) {
+            return {
+                message: 'Введите число',
+            };
+        }
+        const regex = /^[1-9]\d{3}$/
+        if (!regex.test(value)) {
+            return {
+                message: rules.range.message,
+            }
+        }
+        const num = Number(value);
+        const min = rules.range.min;
+        const max = rules.range.max;
+        if (!Number.isInteger(num) || num < min || num > max) {
+            return {
+                message: rules.range.message,
+            }
         }
     }
 
