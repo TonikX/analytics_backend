60da3c16973db9f82432f5ef637429e3b4fd1351
rendex85 <rendex85@gmail.com>
2022-07-31 20:13:42 +0300

    email url+redirect

--- application/analytics_project/settings.py
+++ application/analytics_project/settings.py
@@ -154,9 +154,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static-backend/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-backend')
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
