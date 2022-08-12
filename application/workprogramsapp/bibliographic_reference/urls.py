3de4e4c60e4411ea3de25e77a7cbf0f768baec0a
rendex85 <rendex85@gmail.com>
2022-08-12 15:48:08 +0300

    bibliographic reference with wp id endpoint

--- application/workprogramsapp/bibliographic_reference/urls.py
+++ application/workprogramsapp/bibliographic_reference/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import SearchInEBSCO, BibliographicReferenceViewSet
+from .views import SearchInEBSCO, BibliographicReferenceViewSet, BibliographicReferenceCreateWithWpView
 
 router = DefaultRouter()
 router.register(r'api/workprogram_sources/bibliographic_reference', BibliographicReferenceViewSet,
@@ -10,6 +10,8 @@ router.register(r'api/workprogram_sources/bibliographic_reference', Bibliographi
 urlpatterns = [
     path('api/workprogram_sources/search_in_ebsco', SearchInEBSCO),
     url(r'^', include(router.urls)),
+    path('api/workprogram_sources/bibliographic_reference/create_with_wp',
+         BibliographicReferenceCreateWithWpView.as_view())
     # path('api/source_in_workprogram/<int:workprogram_id>',
     #      WorkProgramSourceInWorkProgramList.as_view())
 
