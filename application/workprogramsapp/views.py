ede23b49e66518b8ee5a03391f71ef7c69c40b87
TonikX <antongovorov@gmail.com>
2022-09-22 20:43:21 +0300

    fix endpoints for competention save in rpd

--- application/workprogramsapp/views.py
+++ application/workprogramsapp/views.py
@@ -1338,7 +1338,7 @@ class WorkProgramInFieldOfStudyForWorkProgramForGHList(generics.ListAPIView):
         """
         queryset = WorkProgramInFieldOfStudy.objects.filter(
             work_program__id = self.kwargs['workprogram_id'],
-            work_program_change_in_discipline_block_module__discipline_block_module__descipline_block__academic_plan__academic_plan_in_field_of_study__general_characteristics_in_educational_program = self.kwargs['gh_id']).distinct()            ).distinct()
+            work_program_change_in_discipline_block_module__discipline_block_module__descipline_block__academic_plan__academic_plan_in_field_of_study__general_characteristics_in_educational_program = self.kwargs['gh_id']).distinct()
         serializer = WorkProgramInFieldOfStudyForCompeteceListSerializer(queryset, many=True)
         return Response(serializer.data)
         try:
