2ff9a6dc9baf0b0092634645375fd3e53af92e92
rendex85 <rendex85@gmail.com>
2022-11-15 19:00:34 +0300

    merge

---
+++ frontend/src/containers/EducationalPlan/TrainingModules/AddEducationalProgramModal/types.d.ts
@@ -0,0 +1,9 @@
+import {WithStyles} from "@material-ui/core";
+
+import styles from "./AddEducationalProgramModal.styles";
+import {mapDispatchToProps, mapStateToProps} from "./AddEducationalProgramModal.connect";
+
+export interface TrainingModuleCreateModalProps extends PropsFromRedux, ActionsFromRedux, WithStyles<typeof styles> {}
+
+export type PropsFromRedux = ReturnType<mapStateToProps>;
+export type ActionsFromRedux = ReturnType<mapDispatchToProps>;
