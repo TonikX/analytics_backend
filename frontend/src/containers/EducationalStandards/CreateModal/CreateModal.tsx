b66cfe532aaa40e5e01545c1e3a94fbe012d3915
poline <krasnova.poline@gmail.com>
2022-11-27 19:40:47 +0300

    fix bug with educational standard year

--- frontend/src/containers/EducationalStandards/CreateModal/CreateModal.tsx
+++ frontend/src/containers/EducationalStandards/CreateModal/CreateModal.tsx
@@ -117,7 +117,7 @@ class CreateModal extends React.PureComponent<EducationalStandardCreateModalProp
                       onChange={this.changeYear}
                       format={YEAR_DATE_FORMAT}
                       minDate={'1984'}
-                      maxDate={currentDate.getFullYear().toString()}
+                      maxDate={'2026'}
                     />
                 </DialogContent>
                 <DialogActions>
