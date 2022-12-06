bfed35cd24d9a40da2445a312d8dd5ca492c9d02
TonikX <antongovorov@gmail.com>
2022-12-06 13:07:26 +0300

    fix isu endpoints

--- application/workprogramsapp/isu_merge/academic_plan_update/isu_service.py
+++ application/workprogramsapp/isu_merge/academic_plan_update/isu_service.py
@@ -17,7 +17,7 @@ class IsuService:
         self.isu_user = isu_user
         self.auth_url = "https://id.itmo.ru/auth/realms/itmo/protocol/openid-connect/token"
         self.academic_plan_url = "https://disc.itmo.su/api/v1/academic_plans"
-        self.academic_plan_headers_url = "https://dev.disc.itmo.su/api/v1/academic_plans_heading"
+        self.academic_plan_headers_url = "https://disc.itmo.su/api/v1/academic_plans_heading"
         self.grant_type = "client_credentials"
 
     def get_access_token(self):
