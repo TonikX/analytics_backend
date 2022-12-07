db590427c7bc1b9d8ebf8eac3ea893da2ec9d472
TonikX <antongovorov@gmail.com>
2022-12-07 10:37:27 +0300

    fix permissions for giф

--- application/gia_practice_app/GIA/serializers.py
+++ application/gia_practice_app/GIA/serializers.py
@@ -43,8 +43,9 @@ class GIASerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         editors = validated_data.pop('editors', None)
         gia = GIA.objects.create(**validated_data)
-        gia.editors.set(editors)
+
         if editors:
+            gia.editors.set(editors)
             gia.editors.add(request.user)
 
         if not gia.gia_base:
