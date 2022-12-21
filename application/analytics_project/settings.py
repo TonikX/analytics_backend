fc18ddb97c18b5a7538137afed323565512261dd
TonikX <antongovorov@gmail.com>
2022-12-22 02:15:16 +0300

    fix settings debug

--- application/analytics_project/settings.py
+++ application/analytics_project/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()  # reading .env file
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env('DEBUG') == 'TRUE'
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 100000
 
