23384322fa1e9332c719b808e90ca3a436ebdd21
poline <krasnova.poline@gmail.com>
2022-12-26 16:40:07 +0300

    add fixes

--- frontend/src/containers/EducationalPlan/TrainingModules/DetailTrainingModule/DetailTrainingModule.tsx
+++ frontend/src/containers/EducationalPlan/TrainingModules/DetailTrainingModule/DetailTrainingModule.tsx
@@ -232,19 +232,19 @@ class DetailTrainingModule extends React.Component<DetailTrainingModuleProps> {
                   <TableCell>
                     {type === OPTIONALLY ? '-' : '+'}
                   </TableCell>
-                  {canEdit &&
+                  {canEdit && level === -1 &&
                       <TableCell className={classes.actions}>
-                          {/*<Tooltip*/}
-                          {/*    title={`Удалить ${get(workPrograms, 'length', 0) > 1 ? 'комплект рабочих программ' : 'рабочую программу'}`}>*/}
-                          {/*    <DeleteIcon className={classes.deleteIcon}*/}
-                          {/*                onClick={this.handleClickBlockDelete(blockOfWorkProgram[BlocksOfWorkProgramsFields.ID], get(workPrograms, 'length', 0))}*/}
-                          {/*    />*/}
-                          {/*</Tooltip>*/}
-                          {/*<Tooltip*/}
-                          {/*    title={`Изменить ${get(workPrograms, 'length', 0) > 1 ? 'комплект рабочих программ' : 'рабочую программу'}`}>*/}
-                          {/*    <EditIcon*/}
-                          {/*        onClick={this.handleOpenDetailModal(blockOfWorkProgram)}/>*/}
-                          {/*</Tooltip>*/}
+                          <Tooltip
+                              title={`Удалить ${get(workPrograms, 'length', 0) > 1 ? 'комплект рабочих программ' : 'рабочую программу'}`}>
+                              <DeleteIcon className={classes.deleteIcon}
+                                          onClick={this.handleClickBlockDelete(blockOfWorkProgram[BlocksOfWorkProgramsFields.ID], get(workPrograms, 'length', 0))}
+                              />
+                          </Tooltip>
+                          <Tooltip
+                              title={`Изменить ${get(workPrograms, 'length', 0) > 1 ? 'комплект рабочих программ' : 'рабочую программу'}`}>
+                              <EditIcon
+                                  onClick={this.handleOpenDetailModal(blockOfWorkProgram)}/>
+                          </Tooltip>
                       </TableCell>
                   }
                 </TableRow>
@@ -293,7 +293,7 @@ class DetailTrainingModule extends React.Component<DetailTrainingModuleProps> {
   renderModule = (item: any, level: number, allChild: any, fatherId: number): any => {
     const {classes, canEdit} = this.props
     const blockOfWorkPrograms = item?.change_blocks_of_work_programs_in_modules
-
+    console.log('level', level)
     return(
       <>
         <TableRow>
@@ -310,18 +310,18 @@ class DetailTrainingModule extends React.Component<DetailTrainingModuleProps> {
           {canEdit && (
             <TableCell style={{ height: '40px'}}>
               <div className={classes.moduleButtons}>
-                {/*{item?.childs?.length === 0 ? (*/}
-                {/*    <Button size="small" onClick={this.handleCreateNewWPBlock(item.id)}>*/}
-                {/*      <AddIcon/> РПД*/}
-                {/*    </Button>*/}
-                {/*  ) : <></>*/}
-                {/*}*/}
-                {/*{blockOfWorkPrograms?.length === 0 ? (*/}
-                {/*    <Button size="small" onClick={this.handleAddNewModule(item.id, item?.childs)}>*/}
-                {/*      <AddIcon/> Модуль*/}
-                {/*    </Button>*/}
-                {/*  ) : <></>*/}
-                {/*}*/}
+                {item?.childs?.length === 0 && level === 0 ? (
+                    <Button size="small" onClick={this.handleCreateNewWPBlock(item.id)}>
+                      <AddIcon/> РПД
+                    </Button>
+                  ) : <></>
+                }
+                {blockOfWorkPrograms?.length === 0 && level === -1 ? (
+                    <Button size="small" onClick={this.handleAddNewModule(item.id, item?.childs)}>
+                      <AddIcon/> Модуль
+                    </Button>
+                  ) : <></>
+                }
                 <Tooltip
                   title={`Открепить модуль`}>
                   <DeleteIcon className={classes.deleteIcon}
