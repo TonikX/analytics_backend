29777c64fb513c7a43d3f4db1d7f7ad6cce6ade6
rendex85 <rendex85@gmail.com>
2023-02-01 18:25:08 +0300

    lang ru by default

---
+++ application/workprogramsapp/isu_merge/filterset.py
@@ -0,0 +1,15 @@
+import django_filters
+
+from workprogramsapp.models import IsuObjectsSendLogger
+
+
+class HistoryFilter(django_filters.FilterSet):
+    class Meta:
+        model = IsuObjectsSendLogger
+        fields = {
+            'date_of_sending': ['exact',],
+            'error_status': ['gt', 'exact'],
+            'obj_id': ['exact'],
+            'obj_type': ['exact'],
+
+        }
