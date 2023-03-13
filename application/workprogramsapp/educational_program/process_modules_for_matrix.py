e2ae2663021115226ce29e0cd0c273e6b89ce1a8
rendex85 <rendex85@gmail.com>
2023-03-13 13:34:03 +0300

    practice field added to competence matrix

--- application/workprogramsapp/educational_program/process_modules_for_matrix.py
+++ application/workprogramsapp/educational_program/process_modules_for_matrix.py
@@ -1,18 +1,21 @@
+from gia_practice_app.GIA.models import GIA
+from gia_practice_app.Practice.models import Practice
+from gia_practice_app.Practice.serializers import PracticeCompetenceSerializer
 from workprogramsapp.educational_program.serializers import WorkProgramCompetenceIndicatorSerializer
 from workprogramsapp.models import WorkProgramChangeInDisciplineBlockModule, WorkProgram
 
 
-def recursion_module_matrix(block_module, unique_wp):
+def recursion_module_matrix(block_module, unique_wp, unique_gia, unique_practice):
     block_module_dict = {"name": block_module.name, "type": block_module.type,
                          "change_blocks_of_work_programs_in_modules": [], "childs": []}
     # block_dict["modules_in_discipline_block"].append(block_module_dict)
     if block_module.childs.all().exists():
         for child in block_module.childs.all():
-            block_module_dict["childs"].append(recursion_module_matrix(child, unique_wp))
+            block_module_dict["childs"].append(recursion_module_matrix(child, unique_wp, unique_gia, unique_practice))
     for change_block in WorkProgramChangeInDisciplineBlockModule.objects.filter(
             discipline_block_module=block_module):
         change_block_dict = {"change_type": change_block.change_type,
-                             "credit_units": change_block.credit_units, "work_program": []}
+                             "credit_units": change_block.credit_units, "work_program": [], "practice": [], 'gia': []}
         # block_module_dict["change_blocks_of_work_programs_in_modules"].append(change_block_dict)
         for work_program in WorkProgram.objects.filter(work_program_in_change_block=change_block):
             if work_program.id not in unique_wp:
@@ -23,6 +26,15 @@ def recursion_module_matrix(block_module, unique_wp):
             else:
                 pass
                 # print(work_program)
-        if change_block_dict["work_program"]:
+        for practice in Practice.objects.filter(practice_in_change_block=change_block):
+            if practice.id not in unique_practice:
+                serializer = PracticeCompetenceSerializer(practice)
+                change_block_dict['practice'].append(serializer.data)
+                unique_practice.append(practice.id)
+            else:
+                pass
+                # print(work_program)
+
+        if change_block_dict["work_program"] or change_block_dict["practice"]:
             block_module_dict["change_blocks_of_work_programs_in_modules"].append(change_block_dict)
     return block_module_dict
