6f412e8a4dc89778575de56cc378b67d39a19db1
PolinaShneider <beatrixlinkoln@gmail.com>
2023-04-03 21:18:25 +0200

    Update TS and react-scripts

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,17 +1,35 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  if (!config.plugins) {
-    config.plugins = [];
-  }
+    //do stuff with the webpack config...
+    if (!config.plugins) {
+        config.plugins = [];
+    }
 
-  config.plugins.push(
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/ckeditor_plugins/openlink', to: 'openlink' }
-      ]
-    })
-  )
-  return config;
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        assert: require.resolve('assert'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve("os-browserify"),
+        url: require.resolve('url'),
+        vm: require.resolve('vm-browserify')
+    });
+    config.resolve.fallback = fallback;
+
+    config.plugins.push(
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: 'src/ckeditor_plugins/openlink', to: 'openlink'}
+            ]
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer']
+        })
+    )
+    return config;
 }
--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import configureMockStore from 'redux-mock-store';
 import { render } from '@testing-library/react';
 import App from './App';
 
+const mockStore = (state = {}) => configureMockStore()(state);
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App store={mockStore} />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
