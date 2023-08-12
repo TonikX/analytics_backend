244c8f1b9db09a229e96c6c638d52fba306012af
Polina Shneider <beatrixlinkoln@gmail.com>
2023-08-12 23:38:45 +0200

    Убрала кнопку "Создать учебную сущность"

--- frontend/src/containers/WorkProgram/Prerequisites/CreateModal/CreateModal.tsx
+++ frontend/src/containers/WorkProgram/Prerequisites/CreateModal/CreateModal.tsx
@@ -1,8 +1,8 @@
-import React, {ReactText} from 'react';
+import React, { ReactText } from 'react';
 import get from "lodash/get";
-import {shallowEqualObjects} from "shallow-equal";
+import { shallowEqualObjects } from "shallow-equal";
 
-import {CreateModalProps} from './types';
+import { CreateModalProps } from './types';
 
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -15,20 +15,20 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Tooltip from "@mui/material/Tooltip";
-import {withStyles} from '@mui/styles';
+import { withStyles } from '@mui/styles';
 
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 import SearchSelector from '../../../../components/SearchSelector';
 
-import {fields, PrerequisiteFields} from '../../enum';
-import {TrainingEntitiesFields} from "../../../TrainingEntities/enum";
+import { fields, PrerequisiteFields } from '../../enum';
+import { TrainingEntitiesFields } from "../../../TrainingEntities/enum";
 
 import connect from './CreateModal.connect';
 import styles from './CreateModal.styles';
-import {SubjectAreaFields} from "../../../SubjectArea/enum";
-import {Link} from "react-router-dom";
-import {appRouter} from "../../../../service/router-service";
+import { SubjectAreaFields } from "../../../SubjectArea/enum";
+import { Link } from "react-router-dom";
+import { appRouter } from "../../../../service/router-service";
 import Typography from "@mui/material/Typography";
 
 class CreateModal extends React.PureComponent<CreateModalProps> {
@@ -49,9 +49,9 @@ class CreateModal extends React.PureComponent<CreateModalProps> {
     }
 
     componentDidUpdate(prevProps: Readonly<CreateModalProps>, prevState: Readonly<{}>, snapshot?: any) {
-        const {prerequisite} = this.props;
+        const { prerequisite } = this.props;
 
-        if (!shallowEqualObjects(prerequisite, prevProps.prerequisite)){
+        if (!shallowEqualObjects(prerequisite, prevProps.prerequisite)) {
             const subjectAreaId = get(prerequisite, [PrerequisiteFields.ITEM, TrainingEntitiesFields.SUBJECT_AREA, SubjectAreaFields.ID], null);
 
             this.props.trainingEntitiesActions.changeSubjectId(subjectAreaId);
@@ -72,9 +72,9 @@ class CreateModal extends React.PureComponent<CreateModalProps> {
     }
 
     handleSave = () => {
-        const {prerequisite} = this.state;
+        const { prerequisite } = this.state;
 
-        if (prerequisite[PrerequisiteFields.ID]){
+        if (prerequisite[PrerequisiteFields.ID]) {
             this.props.actions.changePrerequisite(this.state.prerequisite);
         } else {
             this.props.actions.addPrerequisite(this.state.prerequisite);
@@ -82,7 +82,7 @@ class CreateModal extends React.PureComponent<CreateModalProps> {
     }
 
     changeMasterLevelField = (e: React.ChangeEvent) => {
-        const {prerequisite} = this.state;
+        const { prerequisite } = this.state;
 
         this.setState({
             prerequisite: {
@@ -93,7 +93,7 @@ class CreateModal extends React.PureComponent<CreateModalProps> {
     }
 
     saveTrainingEntityField = (value: ReactText) => {
-        const {prerequisite} = this.state;
+        const { prerequisite } = this.state;
 
         this.setState({
             prerequisite: {
@@ -107,7 +107,7 @@ class CreateModal extends React.PureComponent<CreateModalProps> {
     }
 
     changeSubjectAreaField = (value: ReactText) => {
-        const {prerequisite} = this.state;
+        const { prerequisite } = this.state;
 
         this.props.trainingEntitiesActions.changeSubjectId(value);
         this.props.trainingEntitiesActions.getTrainingEntities();
@@ -136,8 +136,8 @@ class CreateModal extends React.PureComponent<CreateModalProps> {
     }
 
     render() {
-        const {isOpen, classes, trainingEntities, subjectArea} = this.props;
-        const {prerequisite} = this.state;
+        const { isOpen, classes, trainingEntities, subjectArea } = this.props;
+        const { prerequisite } = this.state;
 
         const disableButton = get(prerequisite, [PrerequisiteFields.ITEM, TrainingEntitiesFields.ID], '').length === 0;
 
@@ -157,11 +157,9 @@ class CreateModal extends React.PureComponent<CreateModalProps> {
                         <Tooltip
                             title={
                                 <span style={{ fontSize: '13px' }}>
-                                    Пререквезит - объект, отражающий конкретное знание из конкретной области 
+                                    Пререквезит - объект, отражающий конкретное знание из конкретной области
                                     (далее "учебная сущность"), которое должно быть у студента перед началом изучения курса.
-                                    <br /><br />Для добавления необходимо выбрать предметную область и учебную сущность в ней. 
-                                    <br /><br />Если необходимо создать новую учебную сущность, необходимо нажать кнопку "Создать учебную сущность"
-                                     и создать ее в соответствующем интерфейсе.
+                                    <br /><br />Для добавления необходимо выбрать предметную область и учебную сущность в ней.
                                 </span>
                             }
                         >
@@ -173,8 +171,8 @@ class CreateModal extends React.PureComponent<CreateModalProps> {
                     <FormControl component="fieldset">
                         <FormLabel component="legend">Уровень освоения *</FormLabel>
                         <RadioGroup className={classes.radioGroup}
-                                    onChange={this.changeMasterLevelField}
-                                    value={prerequisite[PrerequisiteFields.ITEM]}
+                            onChange={this.changeMasterLevelField}
+                            value={prerequisite[PrerequisiteFields.ITEM]}
                         >
                             <FormControlLabel value="1" control={<Radio checked={prerequisite[PrerequisiteFields.MASTER_LEVEL] === '1'} />} label="Начальный" />
                             <FormControlLabel value="2" control={<Radio checked={prerequisite[PrerequisiteFields.MASTER_LEVEL] === '2'} />} label="Средний" />
@@ -192,31 +190,22 @@ class CreateModal extends React.PureComponent<CreateModalProps> {
                     {/*/>*/}
 
                     <SearchSelector label="Учебная сущность *"
-                                            changeSearchText={this.handleChangeTrainingEntitySearchText}
-                                            list={trainingEntities}
-                                            changeItem={this.saveTrainingEntityField}
-                                            value={get(prerequisite, [PrerequisiteFields.ITEM, TrainingEntitiesFields.ID], '')}
-                                            valueLabel={get(prerequisite, [PrerequisiteFields.ITEM, TrainingEntitiesFields.TITLE], '')}
+                        changeSearchText={this.handleChangeTrainingEntitySearchText}
+                        list={trainingEntities}
+                        changeItem={this.saveTrainingEntityField}
+                        value={get(prerequisite, [PrerequisiteFields.ITEM, TrainingEntitiesFields.ID], '')}
+                        valueLabel={get(prerequisite, [PrerequisiteFields.ITEM, TrainingEntitiesFields.TITLE], '')}
                     />
                 </DialogContent>
                 <DialogActions className={classes.actions}>
-                    <Link to={appRouter.getTrainingEntitiesRoute()}
-                          className={classes.link}
-                          target="_blank"
-                    >
-                        <Typography>
-                            Создать учебную сущность
-                        </Typography>
-                    </Link>
-
                     <Button onClick={this.handleClose}
-                            variant="text">
+                        variant="text">
                         Отмена
                     </Button>
                     <Button onClick={this.handleSave}
-                            variant="contained"
-                            disabled={disableButton}
-                            color="primary">
+                        variant="contained"
+                        disabled={disableButton}
+                        color="primary">
                         Сохранить
                     </Button>
                 </DialogActions>
