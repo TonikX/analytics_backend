76eeae6c339a976fde37edb9f3e3563621a57950
rendex <rendex85@gmail.com>
2023-10-19 13:31:33 +0300

    zun restoration in practices

--- application/workprogramsapp/management/commands/restore_zun_practices.py
+++ application/workprogramsapp/management/commands/restore_zun_practices.py
@@ -3,7 +3,9 @@ import os
 
 from django.core.management import BaseCommand
 
-from workprogramsapp.models import AcademicPlan, Zun, WorkProgramInFieldOfStudy, DisciplineBlockModule
+from gia_practice_app.Practice.models import ZunPractice
+from workprogramsapp.models import AcademicPlan, Zun, WorkProgramInFieldOfStudy, DisciplineBlockModule, \
+    PracticeInFieldOfStudy
 from workprogramsapp.serializers import ZunForManyCreateSerializer
 
 
@@ -11,7 +13,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         dirname = os.path.dirname(__file__)
-        filename = os.path.join(dirname, 'files/RESTORED_ZUNS.json')
+        filename = os.path.join(dirname, 'files/RESTORED_ZUNS_PRACTICE.json')
         f = open(filename)
         data = json.load(f)
         skipped_counter = 0
@@ -20,34 +22,34 @@ class Command(BaseCommand):
         restored_counter = 0
         more_than_one_fs_count = 0
         for restored_data in data:
-            wp_in_fss = WorkProgramInFieldOfStudy.objects.filter(work_program=restored_data["wp_id"],
+            prac_in_fss = PracticeInFieldOfStudy.objects.filter(practice=restored_data["practice_id"],
                                                                  work_program_change_in_discipline_block_module__discipline_block_module__id=
                                                                  restored_data["module_id"]).distinct()
-            if wp_in_fss.count() == 0:
-                print(wp_in_fss)
+            if prac_in_fss.count() == 0:
+                print(prac_in_fss)
                 continue
             restored_counter += 1
-            if wp_in_fss.count() > 1:
+            if prac_in_fss.count() > 1:
                 more_than_one_fs_count += 1
-            wp_in_fs = None
-            for wp_in_fs_needed in wp_in_fss:
+            prac_in_fs = None
+            for prac_in_fs_needed in prac_in_fss:
                 break_this_cycle = False
-                zuns = Zun.objects.filter(wp_in_fs=wp_in_fs_needed)
+                zuns = ZunPractice.objects.filter(practice_in_fs=prac_in_fs_needed)
                 if zuns.exists():
                     zuns_list_to_find = [zun.id for zun in zuns]
                     for restored_zun_id in restored_data["zuns"]:
                         if restored_zun_id in zuns_list_to_find:
-                            wp_in_fs = wp_in_fs_needed
+                            prac_in_fs = prac_in_fs_needed
                             break_this_cycle = True
                             break
                     if break_this_cycle:
                         break
-            if not wp_in_fs:
-                wp_in_fs = wp_in_fss.first()
+            if not prac_in_fs:
+                prac_in_fs = prac_in_fss.first()
 
-            zuns = Zun.objects.filter(wp_in_fs=wp_in_fs)
+            zuns = ZunPractice.objects.filter(practice_in_fs=prac_in_fs)
             zuns_fs_list = [zun.id for zun in zuns]
-            if wp_in_fs.id == restored_data["wp_in_fs_id"] and set(zuns_fs_list) == set(restored_data["zuns"]):
+            if prac_in_fs.id == restored_data["prac_in_fs_id"] and set(zuns_fs_list) == set(restored_data["zuns"]):
                 #print("skipped")
                 skipped_counter += 1
                 continue
@@ -55,12 +57,12 @@ class Command(BaseCommand):
                 for zun_id in restored_data["zuns"]:
                     if zun_id not in zuns_fs_list:
                         try:
-                            zun_to_connect = Zun.objects.get(id=zun_id)
+                            zun_to_connect = ZunPractice.objects.get(id=zun_id)
                         except Zun.DoesNotExist:
                             #print(zun_id)
                             not_exisisted_zuns += 1
                             continue
-                        zun_to_connect.wp_in_fs = wp_in_fs
+                        zun_to_connect.wp_in_fs = prac_in_fs
                         zun_to_connect.save()
                         recreated_counter += 1
                         #print(zun_to_connect, wp_in_fs)
