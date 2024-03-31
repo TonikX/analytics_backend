9dce13f3572a7281414e3d0059865ab3dacbcab3
EvilPug <3608690@gmail.com>
2024-03-31 17:29:03 +0300

    chore(user_management): Файлы отформатированы по black

--- application/workprogramsapp/user_management/views.py
+++ application/workprogramsapp/user_management/views.py
@@ -1,11 +1,8 @@
-# Сериализаторы
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics
 from rest_framework.permissions import IsAuthenticated
 
-# Права доступа
 from dataprocessing.models import User
-# Сериализаторы
 from workprogramsapp.user_management.serializers import UserSerializer
 
 
@@ -13,8 +10,19 @@ class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
-    search_fields = ['username', 'first_name', 'last_name', 'email', 'isu_number', 'groups__name']
+    filter_backends = [
+        filters.SearchFilter,
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    search_fields = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "isu_number",
+        "groups__name",
+    ]
 
 
 class UserRetrieveView(generics.RetrieveUpdateDestroyAPIView):
