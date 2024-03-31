34751afe3aa59b40ed38097c4326d3e02401e873
EvilPug <3608690@gmail.com>
2024-03-31 17:19:17 +0300

    chore(op_selection): Аннотированы и приведены типы

--- application/workprogramsapp/op_slection/temp__skills_array.py
+++ application/workprogramsapp/op_slection/temp__skills_array.py
@@ -1,10 +1,10 @@
-def skill_sorter(skills_frontend, num_of_prof):
-    skills_frontend=dict(skills_frontend)
+def skill_sorter(skills_frontend: dict, num_of_prof: int) -> tuple[list, list]:
+    skills_frontend = dict(skills_frontend)
     key_skills = []
     additional_skills = []
     for key, value in skills_frontend.items():
-        if value / num_of_prof > 0.01:
-            if value / num_of_prof > 0.2:
+        if float(value) / num_of_prof > 0.01:
+            if float(value) / num_of_prof > 0.2:
                 key_skills.append(key)
             else:
                 additional_skills.append(key)
