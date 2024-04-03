3a59951f205b1f55dfe98f156d04f59cc730311b
EvilPug <3608690@gmail.com>
2024-04-03 14:59:38 +0300

    fix(yasg): Исправлен путь до swagger_docs

--- application/analytics_project/yasg.py
+++ application/analytics_project/yasg.py
@@ -16,7 +16,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     re_path(
-        "swagger-docs(?P<format>\.json|\.yaml)",
+        "swagger-docs(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
