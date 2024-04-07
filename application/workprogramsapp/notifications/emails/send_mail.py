087002c464fb2124bea15fb14fa2a0eeb4e6fd85
EvilPug <3608690@gmail.com>
2024-04-07 23:39:50 +0300

    chore(notifications): Оптимизирован код send_mail

--- application/workprogramsapp/notifications/emails/send_mail.py
+++ application/workprogramsapp/notifications/emails/send_mail.py
@@ -11,8 +11,7 @@ from workprogramsapp.notifications.emails.models import SentMail
 def mail_sender(
     topic: str, text: str, emails: list, users: QuerySet
 ) -> Union[SentMail, None]:
-    print(emails)
-    if env("EMAIL_ENABLE") == "True":
+    if env("EMAIL_ENABLE"):
         send_mail(
             topic,
             text,
