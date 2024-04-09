c2526938fb33f3be97648d4402da8455bf5fb819
EvilPug <3608690@gmail.com>
2024-04-09 20:51:22 +0300

    refactor(workprogramsapp): Удален неиспользуемый файл service.py

--- application/workprogramsapp/service.py
+++
@@ -1,6 +0,0 @@
-# class OnlineCourseFilter(filters.FilterSet):
-#     title = CharFilterInFilter(field_name='title', lookup_expr='in')
-#
-#     class Meta:
-#         model = OnlineCourse
-#         fields = ['title']
